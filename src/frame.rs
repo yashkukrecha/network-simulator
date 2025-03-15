7e320e2cc89b528baff01d084539b410f6aa237e
Yash Kukrecha <ykukrecha@gmail.com>
2025-03-15 16:28:01 -0500

    added more to host

--- src/frame.rs
+++ src/frame.rs
@@ -9,6 +9,9 @@ pub struct Frame {
 }
 
 impl Frame {
+
+    // We do not want the frame to own the addresses
+    // We want the frame to own the data
     pub fn new(src_mac: &str, dest_mac: &str, src_ip: &str, dest_ip: &str, data: Vec<u8>, is_arp: bool) -> Self {
         Self {
             src_mac: src_mac.to_string(),
@@ -17,6 +20,17 @@ impl Frame {
             dest_ip: dest_ip.to_string(),
             data,
             is_arp,
+        }
+    }
+
+    pub fn rebuild_L3(self, src_mac: &str, dest_mac: &str) -> Self {
+        Self {
+            src_mac: src_mac.to_string(),
+            dest_mac: dest_mac.to_string(),
+            src_ip: self.src_ip,
+            dest_ip: self.dest_ip,
+            data: self.data,
+            is_arp: self.is_arp
         }
     }
 }
